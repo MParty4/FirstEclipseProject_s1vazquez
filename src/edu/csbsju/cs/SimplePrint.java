bbe3e04058b3f0cb47100795c1735698ce16255e
s1vazquez <s1vazquez@viewlincsci05.ad.csbsju.edu>
2017-02-27 21:13:31 -0600

    this is me saying my prayers ahhh

---
+++ src/edu/csbsju/cs/SimplePrint.java
@@ -0,0 +1,62 @@
+/**
+ * 
+ */
+package edu.csbsju.cs;
+
+/**
+ * @author s1vazquez
+ *
+ */
+public class SimplePrint {
+	private String pName;
+	private int pAge;
+	/**
+	 * 
+	 */
+	public SimplePrint() {
+		// TODO Auto-generated constructor stub
+		
+	}
+
+	/**
+	 * @return the pName
+	 */
+	public String getpName() {
+		return pName;
+	}
+
+	/**
+	 * @param pName the pName to set
+	 */
+	public void setpName(String pName) {
+		this.pName = pName;
+	}
+
+	/**
+	 * @return the pAge
+	 */
+	public int getpAge() {
+		return pAge;
+	}
+
+	/**
+	 * @param pAge the pAge to set
+	 */
+	public void setpAge(int pAge) {
+		this.pAge = pAge;
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("what's up Tre B]!!");
+		for(int i=0; i<args.length;i++){
+			System.out.println("args[ " + i + " ] = " + args[i] );
+			}
+
+	}
+
+}
